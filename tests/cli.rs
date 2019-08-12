7ff07008c285b61866915f69988713bee6370c9f
Piyush Rungta <piyushrungta25@gmail.com>
2019-08-12 21:15:57 +0530

    Add tests

--- tests/cli.rs
+++ tests/cli.rs
@@ -1,13 +1,48 @@
 use std::process::Command;  // Run programs
 use assert_cmd::prelude::*; // Add methods on commands
+use std::fs::File;
+use std::io::Read;
 
-#[test]
-fn hello_world() -> Result<(), Box<std::error::Error>> {
+fn test_helper(file_name: &str) -> Result<(), Box<std::error::Error>> {
+    let mut target_str = String::new();
+    let _ = File::open(format!("{}.out", file_name))?.read_to_string(&mut target_str);
+
     let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME"))?;
-    cmd.arg("samples/hello.bf");
+    cmd.arg(file_name);
     cmd.assert()
         .success()
-        .stdout("Hello World!\n");
+        .stdout(target_str);
+	Ok(())
+}
 
-    Ok(())
+#[test]
+fn test_hello_world() -> Result<(), Box<std::error::Error>> {
+    test_helper("samples/hello_world.bf")
+}
+
+#[test]
+fn test_bang_bang() -> Result<(), Box<std::error::Error>> {
+    test_helper("samples/bangbang.bf")
+}
+
+#[test]
+fn test_99_bottles() -> Result<(), Box<std::error::Error>> {
+    test_helper("samples/bottles.bf")
+}
+
+// run this one with relase build, it takes a long time with debug build
+#[ignore]
+#[test]
+fn test_mandelbrot() -> Result<(), Box<std::error::Error>> {
+    test_helper("samples/mandelbrot.bf")
+}
+
+#[test]
+fn test_quine() -> Result<(), Box<std::error::Error>> {
+    test_helper("samples/quine.bf")
+}
+
+#[test]
+fn test_square_number() -> Result<(), Box<std::error::Error>> {
+    test_helper("samples/square_number.bf")
 }
